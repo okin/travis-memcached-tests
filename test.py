1b09bd8fd48b6cc24439aa076dc93b65096e8d2c
Niko Wenselowski <der@nik0.de>
2018-10-24 18:31:42 +0200

    Testing python-memcached.

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import memcache  # python-memcached
 import umemcache
 
 ADDRESS = "127.0.0.1:11211"
+
+
+def test_memcache_client():
+    print("memcache client creation")
+    client = memcache.Client([ADDRESS], debug=1)
+    print("Client is connected")
+
+    get_value(client)
 
 
 def test_umemcache_client():
@@ -11,8 +20,11 @@ def test_umemcache_client():
     client = umemcache.Client(ADDRESS)
     client.connect()
     print("Client is connected")
+    get_value(client)
 
-    print("Trying to get a value...")
+
+def get_value(client):
+    print("Trying to get a value with {}...".format(client))
     try:
         client.get("")
     except Exception as err:
@@ -20,4 +32,5 @@ def test_umemcache_client():
 
 
 if __name__ == '__main__':
+    test_memcache_client()
     test_umemcache_client()
